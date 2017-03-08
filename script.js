787ed99ba36a3a54a2732ffc20c5e3e8ab9d793c
Pguhan <poojaguhan97@gmail.com>
2017-03-08 23:25:31 +0530

    Update script.js
    name changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function () {
- var names = ["Yaakov","John","Jen", "Paul", "Frank","Larry", "Paula","Laura", "Jim"];
+ var names = ["Ravi","John","Jen", "Paul", "Frank","Larry","Laura","Jim"];
 for (var i=0; i<names.length; i++) {
 
 var firstLetter = names[i].charAt(0).toLowerCase();
